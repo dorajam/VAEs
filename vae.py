c0e73e0b4b8b5c0013b432f462441acaa7c90408
dorajambor <dorajambor@gmail.com>
2016-09-17 18:03:16 +0200

    vae finito

--- vae.py
+++ vae.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 import os
 import sys
 
@@ -53,6 +54,9 @@ class VAE(object):
          self.x_reconstructed, self.z_, self.x_reconstructed_,
          self.cost, self.global_step, self.train_op) = handles
 
+         if save_graph_def:     # tensorboard
+             self.logger = tf.train.SummaryWriter(log_dir, self.sesh.graph)
+
 
     def _buildGraph(self):
         x_in = tf.placeholder(tf.float32, shape=[None, self.architecture[0]], name='x')
@@ -102,9 +106,27 @@ class VAE(object):
 
         # mismatch b/w the extimated and true posterior distrib p(z|x)
         kl_loss = VAE.kullbackLeibler(z_mean, z_log_sigma) 
+
+        with tf.name_scope("cost"):
+            # average over minibatch
+            cost = tf.reduce_mean(rec_loss + kl_loss, name='cost')
+           
+        # training step counter
+        global_step = tf.Variable(0, trainable=False)
+
+        # optimizer obj
+        with tf.name_scope("Adam_optimizer"):
+            optimizer = tf.train.AdamOptimizer(self.learning_rate)
+            tvars = tf.trainable_variables()
+            grads_and_vars = optimizer.compute_gradients(cost,tvars)
+            clipped = [(tf.clip_by_value(grad, -5, 5), tvar 
+                            for grad,tvar in grads_and_vars]
+
+            train_op = optimizer.apply_gradients(clipped, 
+                            global_step = global_step, name='minimize_cost')
         
-
-
+        return (x_in, dropout, z_mean, z_log_sigma, x_generated, 
+                            z_, x_generate_, cost, global_step, train_op)
 
 
     def sampleGaussian(self, mu, log_sigma):
@@ -115,8 +137,6 @@ class VAE(object):
             return mu + epsilon * tf.exp(log_sigma) # N(mu, I * sigma**2)
 
 
-
-					
     @staticmethod
     def crossEntropy(obs, actual, offset=1e-7):
         """Binary cross-entropy, per training example"""
@@ -136,3 +156,88 @@ class VAE(object):
             # = -0.5 * (1 + log(sigma**2) - mu**2 - sigma**2)
             return -0.5 * tf.reduce_sum(1 + 2 * log_sigma - mu**2 -
                                         tf.exp(2 * log_sigma), 1)
+
+   
+   def encode(self, x):
+        ''' Probabilitistic encoder from inputs to latent distrib parameters
+        "the inference net" -> takes input, approximates q(z|x) 
+        '''
+        feed_dict = {self.x_in: x}
+
+        # run all operations and evaluate res in the z params list -> in "fetches"
+        return self.sesh.run([self.z_mean, self.log_sigma], feed_dict=feed_dict)
+
+
+   def decode(self, z_vals = None):
+        ''' Generative decoder, mapping the latent z to the reconstructed x
+        "the generative net p(x|z)
+        '''
+        feed_dict = {}
+        if no z_vals:
+            feed_dict.update({self.z_ = zs})   # decode from calculated z vals
+
+        # else draw from z ~ N(0,I)
+        return self.sesh.run(self.x_generated_, feed_dict=feed_dict)
+
+    
+    def vae(self, x):
+        ''' end-to-end VAE '''
+        # not sure when self.encode would return None ...
+        return self.decode(self.sampleGaussian(*self.encode(x)))
+
+    def train(self, data, max_iter=np.inf, max_epochs, np.inf, cross_validate=True,
+            verbose=True, save=True, outdie='./outdir', plots_outdir='./png',
+            plot_latent_over_time=False):
+        
+        # save checkpoints -> current state of vars
+        if save:
+            saver = tf.train.Saver(tf.all_variables())      # return everything from GraphKeys.VARIABLES
+            
+        try:
+            total_cost = 0
+            now = datetime.now().isoformat()[11:] 
+            print('Training begin: {}'.format(now))
+        
+            while True:
+                x, label = data.train.next_batch(self.batch_size)       # get training samples
+                feed_dict = {self.x_in:x, self.dropout_: self.dropout}
+                fetches = [self.x_generate, self.cost, self.global_step, self.train_op]]
+                x_generated, cost, i, _ = self.sesh.run(fetches, feed_dict)
+
+                total_cost += cost
+
+                if i%1000 and verbose:
+                    print "iteration {}, avg cost {}".format(i,total_cost/i)
+
+                if i >= max_iter or data.train.epochs_completed >= max_epochs:
+                    print "final avg cost {} @ epoch {}".format(total_cost/i, data.train.epochs_completed)
+
+                    now = datetime.now().isoformat()[11:]
+                    print 'Training ends: {}'.format(now)
+
+                    if save:
+                        outfile = os.path.join(os.path.abspath(outdir), "{}_vae_{}".format(
+                                     self.datetime, '_'.join(map(str, self.architecture))))
+                        saver.save(self.sesh, outfile, global_step=self.step)
+
+                    # took this format from Miriam's code, though not sure about 'try'
+                    try:
+                        self.logger.flush()
+                        self.logger.close()
+                    except(AttributeError):
+                        continue
+                    break
+
+
+        except(KeyboardInterrupt, ):          # pressing cmd c to stop
+            print 'final avg cost at step {} is {}'.format(i, data.train.epochs_completed, total_cost/i)
+            now = datetime.now().isoformat()[11:]
+            print 'training ended at {}'.format(now)
+            sys.exit(0)
+            
+
+
+
+
+
+
