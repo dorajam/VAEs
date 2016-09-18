90a23ddc80a7743a914bf4d632230e4db83884fb
dorajambor <dorajambor@gmail.com>
2016-09-18 23:59:22 +0200

    vae finished

--- utils.py
+++ utils.py
@@ -11,4 +11,24 @@ def composeAll(*args):
 
 	return partial(functools.reduce, compose)(*args)
 
+def print_(var, name: str, first_n=5, summarize=5):
+    ''' print values of tf.Vars during training
+    args: 
+    input_ = tensor passed through op
+    data = list of tensors to print out
+    first_n = only the first n times
+    summarize = only print this amount of entries in the tensor'''
+
+    return tf.Print(var, [var], '{}'.format(name), first_n=first_n, summarize=summarize)
 	
+def get_mnist(n, mnist):
+    ''' takes n (int in [0,9]), returns 784D numpy array '''
+    assert 0 <= n <= 9, "n must be in [0-9]"
+    import random
+    SIZE = 500
+    imgs, labels = mnist.train.next_batch(SIZE)
+    imgs.shuffle()
+
+    for i in xrange(len(imgs)):
+        if labels[i] == n:
+            return imgs[i]
